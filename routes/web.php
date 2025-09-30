7be15a9578a9d31bc0119111e0ca373680dc851e
lbz480 <zleandro067@gmail.com>
2025-09-30 08:37:04 -0400

    union splash y productos

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController;
 
+// Página inicial -> splash
 Route::get('/', function () {
     return view('splash');
-});
+})->name('splash');
 
-Route::get('/splash', function () {
-    return view('splash');
-});
+
+// Después del splash, ya entras al CRUD de productos
+Route::get('/productos', [ProductoController::class, 'index'])->name('productos.index');
+Route::post('/productos', [ProductoController::class, 'store'])->name('productos.store');
+Route::put('/productos/{id}', [ProductoController::class, 'update'])->name('productos.update');
+Route::delete('/productos/{id}', [ProductoController::class, 'destroy'])->name('productos.destroy');
