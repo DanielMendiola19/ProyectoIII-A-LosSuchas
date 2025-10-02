6303980e698b2c88e57fe09d13ee5bb243cef252
BotonStop <alfredocortez12god@gmail.com>
2025-10-01 22:21:15 -0400

    Union rutas

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\AuthController;
-=======
 use App\Http\Controllers\ProductoController;
-
->>>>>>> dashboardySplash
 
 
 // Página inicial -> splash
 Route::get('/', function () {
-<<<<<<< HEAD
-    return view('welcome');
-});
+    return view('splash');
+})->name('splash');
+
 
 // Formulario
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login.form');
@@ -34,9 +30,6 @@ Route::get('/bienvenida', function () {
 
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-=======
-    return view('splash');
-})->name('splash');
 
 // Dashboard (se redirige aquí después del splash)
 Route::get('/dashboard', function () {
@@ -48,4 +41,3 @@ Route::get('/productos', [ProductoController::class, 'index'])->name('productos.
 Route::post('/productos', [ProductoController::class, 'store'])->name('productos.store');
 Route::put('/productos/{id}', [ProductoController::class, 'update'])->name('productos.update');
 Route::delete('/productos/{id}', [ProductoController::class, 'destroy'])->name('productos.destroy');
->>>>>>> dashboardySplash
