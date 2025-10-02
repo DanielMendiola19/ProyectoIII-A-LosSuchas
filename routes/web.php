9a0ad316cc9256057670693f07135a8e5a022b89
DanielMendiola19 <dasd05947@gmail.com>
2025-10-01 23:49:35 -0400

    Union Splash , DashBoard, Productos, Navegacion

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,18 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductoController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Página inicial -> splash
+Route::get('/', function () {
+    return view('splash');
+})->name('splash');
 
-/* Route::get('/', function () {
-    return view('welcome');
-}); */
-Route::get('/', [ProductoController::class, 'index'])->name('productos.index');
+// Dashboard (se redirige aquí después del splash)
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+// CRUD de productos
+Route::get('/productos', [ProductoController::class, 'index'])->name('productos.index');
 Route::post('/productos', [ProductoController::class, 'store'])->name('productos.store');
 Route::put('/productos/{id}', [ProductoController::class, 'update'])->name('productos.update');
 Route::delete('/productos/{id}', [ProductoController::class, 'destroy'])->name('productos.destroy');
